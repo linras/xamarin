b035b567a386fa8971f08511f3e614b1bfc1474d
Linras <linras@wp.pl>
2019-03-26 00:32:52 +0100

    dog form

--- MyApp/MyApp/MyApp/DogFormViewModel.cs
+++ MyApp/MyApp/MyApp/DogFormViewModel.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MyApp
 {
     public class DogFormViewModel
     {
-        public DogFormViewModel()
-        {
-            Name = "enter name";
-        }
         public string Name { get; set; }
         public string Race { get; set; }
         public int Age { get; set; }
+        public ICommand SaveCommand { get; set; }
+        public DogFormViewModel()
+        {
+            Name = "default";
+            SaveCommand = new Command(SaveData);
+        }
+
+        private void SaveData()
+        {
+            Page page = new Page();
+            page.DisplayAlert("Inserted dog", Name + Race + Age, "OK");
+        }
+
+
+
     }
 }
